6501fcccdf1512c1dbed33cdaff6978e5ce60e04
Aayushthapa1 <aayush134056@gmail.com>
2024-10-13 22:45:28 +0545

    okk

--- Backend/server.js
+++ Backend/server.js
@@ -4,5 +4,5 @@ import express from "express";
 const app = express();
 
 app.listen(3000, () => 
-    {console.log("Server is running on port ${port}")
+    {console.log("Server is running on port 5000")
 });
