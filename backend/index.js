43255a40ce96e73d7da2b13490c19ce62d6381ea
Aayushthapa1 <aayush134056@gmail.com>
2024-11-25 15:06:37 +0545

    setup done

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -6,10 +11,12 @@ app.get("/", (req, res) => {
     res.send("Hello from backend");
 })
 
+
+app.use("/api/auth",authRoutes);
 app.listen(5001, () => {
+    connectDB();
     console.log("Server is running on port 5001");
 });
 
-//aayush134056
-//DlPXgS71xlnZeRRd
+
 
