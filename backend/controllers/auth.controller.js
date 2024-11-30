b1f63647c2ed4950caf59ea19d634117849bb75c
Aayushthapa1 <aayush134056@gmail.com>
2024-11-30 10:16:37 +0545

    temlplates work done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
+import { sendVerificationEmail , sendWelcomeEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
+
+
     const { name, email, password } = req.body;
 
     try {
@@ -63,6 +65,52 @@ export const signup = async (req, res) => {
             message: error.message,
         });
     }
+};
+
+export const verifyEmail = async function (req, res) {
+    const {code} = req.body;
+
+    try {
+        const user = await User.findOne({verificationToken: code, verificationTokenExpireAt: {$gt: Date.now()}});
+
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid verification code",
+            });
+        }
+
+        if(user.verificationTokenExpireAt < Date.now()){
+            return res.status(400).json({
+                success: false,
+                message: "Verification code has expired",
+            }); 
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpireAt = undefined;
+
+        await user.save();
+
+        await sendWelcomeEmail(user.email, user.name);
+        res.status(200).json({success: true, message: "Email verified successfully"
+            ,user : {
+                ...user._doc,
+                password: undefined
+            }
+        });
+    }
+    catch (error) {
+        console.log("error in verifying email",error);
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+
+
+
 };
 
 export const login = async (req, res) => {
