3c7fc095198d88aca9793baceff7eb60d732fdb2
Aayushthapa1 <aayush134056@gmail.com>
2024-12-03 11:34:12 +0545

    temlplates work done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -191,7 +191,7 @@ export const forgotPassword = async (req, res) => {
         });
     }
     catch (error) {
-        console.log("Error in forgot password",error);
+        console.log("Error in forgot password",errr);
         res.status(400).json({
             success: false,
             message: error.message
